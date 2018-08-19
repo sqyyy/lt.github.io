786b8c6ffea181a6d44159e73b04bc35d0829679
sqyyy <search@gmail.com>
2018-08-19 14:31:35 +0800

    update

--- letao/public/m/js/search.js
+++ letao/public/m/js/search.js
@@ -1,12 +1,11 @@
 $(function () {
-    $('.lt_search a').on('tap',function () {
-        var key=$.trim($('input').val());
-        if (!key){
+    $('.lt_search a').on('tap', function () {
+        var key = $.trim($('input').val());
+        if (!key) {
             mui.toast('请输入搜索内容',
-                { duration:'short', type:'div' })
-        }else{
-            location.href='searchlist.html?key='+key;
+                {duration: 'short', type: 'div'});
+            return false;
         }
-
+        location.href = 'searchlist.html?key=' + key;
     });
 });
